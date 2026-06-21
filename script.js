10433c45fa3da8d2d1eb133e52c171a1e3aa9500
lenro2 <lelegmmglele@gmail.com>
2026-06-21 16:19:13 -0300

    terminar ressucitação

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+function coracao() {
+    const coracao = document.getElementById("coracao");
+    document.
+}
+
 function mataremoji() {
     const emoji = document.getElementById("emoji");
     emoji.src="imagens/EmojiMorto.png";
+    coracao();
 }
