692027b627507de73df150ef2ebf0309092c67c8
lenro2 <lelegmmglele@gmail.com>
2026-06-23 22:18:10 -0300

    coração finalizado

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-// function salvar() {
-//     const coracao = document.getElementById("coracao");
-//     coracao.classList.remove("inv");
-//     coracao.classList.add("img");
-// }
-
 function mataremoji() {
     const emoji = document.getElementById("emoji");
     emoji.src="imagens/EmojiMorto.png";
-    // salvar();
+    const coracao = document.getElementById("coracao");
+    coracao.classList.remove("inv");
+    coracao.classList.add("img");
+    const coracao2 = document.getElementById("coracao2");
+    coracao2.classList.remove("inv");
+    coracao2.classList.add("img");
+}
+
+function salvar() {
+    emoji.src="imagens/EmojiJoinha.png";
+    coracao.classList.remove("img");
+    coracao.classList.add("inv");
+    coracao2.classList.remove("img");
+    coracao2.classList.add("inv");
 }
